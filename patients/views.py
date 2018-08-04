02d2e985eb7472cf2202ed3e76cea3d2b33a17de
LeonidDev <lguadalupe@appen.com>
2018-08-04 23:27:10 +0800

    Moving form class to forms.py file

--- patients/views.py
+++ patients/views.py
@@ -6,7 +6,7 @@ from django.views.generic import ListView
 from django.http import HttpResponse
 
 from patients.models import Patient
-
+from .forms import PatientForm
 # class DateInput(forms.DateInput):
 # 	input_type = 'date'
 
